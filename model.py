a644522d6eebad1fc9135820844882b290875f19
ryanmartin060708-code <ryan.martin060708@gmail.com>
2026-03-29 05:44:20 +0530

    Delete model.py

--- model.py
+++
@@ -1,6 +0,0 @@
-def predict(telemetry):
-    if (telemetry["temperature"] > 85 or
-        telemetry["pressure"] > 120 or
-        telemetry["vibration"] > 0.75):
-        return "anomaly"
-    return "normal"
